ebb68540daf3f6a1d3130f764b487e01851a1fa3
RobbleX <jmroblex@gmail.com>
2016-12-18 13:03:20 +0100

    Añade servicio y elimina ficheros autogenerados

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NotaListComponent } from './ejercicio/nota-list.component';
+import { Anotable }  from './ejercicio/nota.model';
 
 
 @Component({
   selector: 'hola-mundo',
-  template: `<h1>Hola Mundo!</h1><router-outlet></router-outlet>`
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent { }
